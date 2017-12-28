5f24dacf14be8dd38a20818f6fb532cbcf2d65f1
ren <530770675@qq.com>
2017-12-28 14:15:52 +0800

    int

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/*IT License http://www.opensource.org/licenses/mit-license.php
+Author  @renming*/
+var path = require(‘path‘);
+console.log("coodev");//注意这回没写妥妥的
+var arguments = process.argv.splice(2);
+console.log("arguments:", arguments);
