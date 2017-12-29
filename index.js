50b0d9a8ff9f3fa9451e149ab84548198b8a8be1
ren <530770675@qq.com>
2017-12-29 18:39:47 +0800

    ss

--- index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/*IT License http://www.opensource.org/licenses/mit-license.php
-Author  @renming*/
-var path = require(‘path‘);
-console.log("coodev");//注意这回没写妥妥的
-var arguments = process.argv.splice(2);
-console.log("arguments:", arguments);
